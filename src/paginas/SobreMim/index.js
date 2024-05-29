56fff5071e3a708196a0c4eff070a2ec526a71de
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-05-29 13:06:02 -0300

    feat: Post modelo component

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,29 @@
+import PostModelo from "components/PostModelo";
+
+import fotoCapa from "assets/sobre_mim_capa.png";
+import fotoSobreMim from "assets/sobre_mim_foto.jpeg";
+
+import styles from "./SobreMim.module.css";
+
 export default function SobreMim() {
     return (
-        <h1>Sobre mim</h1>
+        <PostModelo
+            fotoCapa={fotoCapa}
+            titulo="Sobre mim"
+        >
+           <h3 className={styles.subtitulo}>Olá, eu sou o Matheus!</h3>
+           <img 
+                src={fotoSobreMim}
+                alt="Foto do Matheus Sorrindo"
+                className={styles.fotoSobreMim}
+           />
+
+           <p className={styles.paragrafo}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet magna eleifend, dictum turpis non, posuere erat. Sed commodo metus et mattis interdum. Mauris tempus neque sed augue pellentesque, in mollis purus blandit. Quisque vel tortor sit amet diam accumsan lobortis. Nam pulvinar bibendum lacus, ac iaculis enim lacinia vel. Fusce ut ligula erat. Vestibulum tempus tortor arcu, ac aliquam elit imperdiet in. Sed justo mi, sollicitudin at felis malesuada, vestibulum consectetur quam. Nam interdum, turpis et tristique tempor, magna velit vestibulum libero, sed varius lacus ex nec erat. Quisque est dolor, condimentum in lacus sed, eleifend sagittis metus. Mauris porttitor dui vel augue iaculis, at pellentesque odio varius. Aenean vitae nisl ut enim venenatis fermentum sit amet in odio. Nunc consequat id arcu vel tempus.</p>
+
+           <p className={styles.paragrafo}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet magna eleifend, dictum turpis non, posuere erat. Sed commodo metus et mattis interdum. Mauris tempus neque sed augue pellentesque, in mollis purus blandit. Quisque vel tortor sit amet diam accumsan lobortis. Nam pulvinar bibendum lacus, ac iaculis enim lacinia vel. Fusce ut ligula erat. Vestibulum tempus tortor arcu, ac aliquam elit imperdiet in. Sed justo mi, sollicitudin at felis malesuada, vestibulum consectetur quam. Nam interdum, turpis et tristique tempor, magna velit vestibulum libero, sed varius lacus ex nec erat. Quisque est dolor, condimentum in lacus sed, eleifend sagittis metus. Mauris porttitor dui vel augue iaculis, at pellentesque odio varius. Aenean vitae nisl ut enim venenatis fermentum sit amet in odio. Nunc consequat id arcu vel tempus.</p>
+
+           <p className={styles.paragrafo}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet magna eleifend, dictum turpis non, posuere erat. Sed commodo metus et mattis interdum. Mauris tempus neque sed augue pellentesque, in mollis purus blandit. Quisque vel tortor sit amet diam accumsan lobortis. Nam pulvinar bibendum lacus, ac iaculis enim lacinia vel. Fusce ut ligula erat. Vestibulum tempus tortor arcu, ac aliquam elit imperdiet in. Sed justo mi, sollicitudin at felis malesuada, vestibulum consectetur quam. Nam interdum, turpis et tristique tempor, magna velit vestibulum libero, sed varius lacus ex nec erat. Quisque est dolor, condimentum in lacus sed, eleifend sagittis metus. Mauris porttitor dui vel augue iaculis, at pellentesque odio varius. Aenean vitae nisl ut enim venenatis fermentum sit amet in odio. Nunc consequat id arcu vel tempus.</p>
+
+        </PostModelo>
     );
 };
