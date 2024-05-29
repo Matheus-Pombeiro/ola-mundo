2547295ac62628faab03379e7f7a3c1c70609357
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-05-29 11:30:01 -0300

    feat: nested routes

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./components/Menu";
+import Rodape from "components/Rodape";
+import PaginaPadrao from "components/PaginaPadrao";
 
 const AppRoutes = () => {
   return (
@@ -10,10 +12,31 @@ const AppRoutes = () => {
       <Menu />
 
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
+        </Route>
+
+        {/* 
+        
+        Na rota "/", a estrutura a ser renderizada é:
+
+        <PaginaPadrao>
+          <Inicio />
+        </PaginaPadrao>
+
+        Na rota "/sobremim", a estrutura a ser renderizada é:
+
+        <PaginaPadrao>
+          <SobreMim />
+        </PaginaPadrao>
+
+        */}
+        
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
+
+      <Rodape />
     </BrowserRouter>
   );
 }
